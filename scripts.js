c1474384811f0723d9fcf27a918c04e036aa36a4
danielsoares13 <danielssoares13@gmail.com>
2024-10-25 20:58:31 -0300

    atualização do modal

--- scripts.js
+++ scripts.js
@@ -2,21 +2,25 @@ let BotaoSom = document.querySelector(".botao-som")
 let video = document.querySelector(".video")
 let Botao = document.querySelector(".link-info")
 let modal = document.querySelector('.modal')
+let toggle = document.getElementById('toggle');
  
 
-BotaoSom.addEventListener("click", LigaSom)
+toggle.addEventListener('change', function() {
+    if (this.checked) {
+        video.muted = false
+    } else {
+        video.muted = true
+    }
+});
 
-function LigaSom(){
-    video.muted = false
-}
-
-Botao.addEventListener("click", MostrarBotao)
+Botao.addEventListener("click", MostrarModal)
 modal.addEventListener("click", EsconderModal)
 
-function MostrarBotao(){
+function MostrarModal(){
     modal.style.display = "block"
 }
 
 function EsconderModal(){
     modal.style.display = "none"
 }
+
