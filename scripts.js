9f2830c4aa90fbca4205cd12c57a5872f9b7d699
danielsoares13 <danielssoares13@gmail.com>
2024-11-04 23:58:22 -0300

    add bottom

--- scripts.js
+++ scripts.js
@@ -2,16 +2,6 @@ let BotaoSom = document.querySelector(".botao-som")
 let video = document.querySelector(".video")
 let Botao = document.querySelector(".link-info")
 let modal = document.querySelector('.modal')
-let toggle = document.getElementById('toggle');
- 
-
-toggle.addEventListener('click', function() {
-    if (this.checked) {
-        video.muted = false
-    } else {
-        video.muted = true
-    }
-});
 
 Botao.addEventListener("click", MostrarModal)
 modal.addEventListener("click", EsconderModal)
@@ -24,3 +14,17 @@ function EsconderModal(){
     modal.style.display = "none"
 }
 
+
+function toggleVolume() {
+    let BotaoSom = document.getElementById("volumeToggle");
+    let icon = BotaoSom.querySelector("i");
+
+    // Verifica o estado atual do áudio
+    if (video.muted) {
+      video.muted = false;
+      icon.classList.replace("fa-volume-mute", "fa-volume-up");
+    } else {
+      video.muted = true;
+      icon.classList.replace("fa-volume-up", "fa-volume-mute");
+    }
+  }
