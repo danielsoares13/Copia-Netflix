9df1006c42e10bd33a20371047ac60e7d21fe7f2
danielsoares13 <danielssoares13@gmail.com>
2024-11-12 22:05:56 -0300

    new features

--- scripts.js
+++ scripts.js
@@ -2,19 +2,30 @@ const video = document.querySelector(".video")
 const VideoModal = document.querySelector(".video-modal")
 const BotaoSom = document.getElementById("volumeToggle");
 const icon = BotaoSom.querySelector("i");
+const BotaoSomModal = document.getElementById("volumeToggleModal");
+const iconModal = BotaoSomModal.querySelector("i");
 const Botao = document.querySelector(".link-info")
 const modal = document.querySelector('.modal')
 
 Botao.addEventListener("click", MostrarModal)
 modal.addEventListener("click", EsconderModal)
 
-function MostrarModal(){
-    modal.style.display = "block"
+
+function MostrarModal(Botao){
+   modal.style.display = "block"
+  if (Botao = true){
+    video.pause();
+  }
 }
 
 function EsconderModal(event) {
   if (event.target === modal) {
     modal.style.display = "none";
+    VideoModal.muted = true;
+    iconModal.classList.replace("fa-volume-up", "fa-volume-mute");
+    VideoModal.currentTime = 0;
+    video.play();
+    video.muted = false;
   }
 };
 
@@ -35,9 +46,9 @@ function toggleVolume() {
     // Verifica o estado atual do áudio
     if (VideoModal.muted) {
       VideoModal.muted = false;
-      icon.classList.replace("fa-volume-mute", "fa-volume-up");
+      iconModal.classList.replace("fa-volume-mute", "fa-volume-up");
     } else {
       VideoModal.muted = true;
-      icon.classList.replace("fa-volume-up", "fa-volume-mute");
+      iconModal.classList.replace("fa-volume-up", "fa-volume-mute");
     }
   }
